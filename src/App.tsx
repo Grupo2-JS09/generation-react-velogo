b47c0c2bdbc848d4260ca2395eef63710d15997a
Maristela Rocha <maristela.rocha.silva@gmail.com>
2025-11-06 10:31:17 -0300

    fix: ListaCategoria

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import DeletarCategoria from "./components/categoria/deletarcategoria/DeletarCategoria";
 import FormCategoria from "./components/categoria/formcategoria/FormCategoria";
-import ListaCategorias from "./components/categoria/listacategoria/ListaCategoria";
+import ListaCategoria from "./components/categoria/listacategoria/ListaCategoria";
 import Home from "./pages/home/Home";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
         <div className="min-h-[80vh]">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/categorias" element={<ListaCategorias />} />
+            <Route path="/categorias" element={<ListaCategoria />} />
             <Route path="/cadastrarcategorias" element={<FormCategoria />} />
             <Route path="/editarcategorias/:id" element={<FormCategoria />} />
             <Route
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "",
+  baseURL: "http://localhost:3000",
 });
 
 export const buscar = async (
