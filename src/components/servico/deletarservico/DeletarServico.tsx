e0a6b5196e0f148aebe5bb3fe031fdd9d82dffb6
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-11-06 09:49:09 -0400

    build: deletar serviço

---
+++ src/components/servico/deletarservico/DeletarServico.tsx
@@ -0,0 +1,113 @@
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
+// import { AuthContext } from "../../../contexts/AuthContext";
+import { buscar, deletar } from "../../../services/Service";
+import type Servico from "../../../models/Servico";
+// import { ToastAlerta } from "../../../utils/ToastAlerta";
+
+function DeletarServico() {
+  const navigate = useNavigate();
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [servico, setServico] = useState<Servico>({} as Servico);
+
+  const { id } = useParams<{ id: string }>();
+
+  // const { usuario, handleLogout } = useContext(AuthContext);
+  // const token = usuario.token;
+
+  async function buscarPorId(id: string) {
+    try {
+      // await buscar(`/servicos/${id}`, setServico, {
+      //   headers: {
+      //     Authorization: token,
+      //   },
+      // });
+      await buscar(`/servicos/${id}`, setServico);
+    } catch (error: any) {
+      if (error.toString().includes("401")) {
+        // handleLogout();
+        navigate("/")
+      }
+    }
+  }
+
+  // useEffect(() => {
+  //   if (token === "") {
+  //     ToastAlerta("Você precisa estar logado", "info");
+  //     navigate("/");
+  //   }
+  // }, [token]);
+
+  useEffect(() => {
+    if (id !== undefined) {
+      buscarPorId(id);
+    }
+  }, [id]);
+
+  async function deletarServico() {
+    setIsLoading(true);
+
+    try {
+      await deletar(`/servicos/${id}`);
+      alert("Serviço apagado com sucesso");
+
+    } catch (error: any) {
+      if (error.toString().includes("401")) {
+        // handleLogout();
+        navigate("/")
+      } else {
+        alert("Erro ao deletar o serviço.");
+      }
+    }
+
+    setIsLoading(false);
+    retornar();
+  }
+
+  function retornar() {
+    navigate("/servicos");
+  }
+
+  return (
+    <div className="container w-1/3 mx-auto">
+      <h1 className="text-4xl text-center my-4">Deletar Serviço</h1>
+
+      <p className="text-center font-semibold mb-4">
+        Você tem certeza de que deseja apagar o serviço a seguir?
+      </p>
+
+      <div className="border flex flex-col rounded-2xl overflow-hidden justify-between">
+        <header className="py-2 px-6 bg-indigo-600 text-white font-bold text-2xl">
+          Serviço
+        </header>
+        <div className="p-4">
+          <p className="text-xl h-full">{servico.modalidade}</p>
+          <p>{servico.valor_mensalidade}</p>
+        </div>
+        <div className="flex">
+          <button
+            className="text-slate-100 bg-red-400 hover:bg-red-600 w-full py-2"
+            onClick={retornar}
+          >
+            Não
+          </button>
+          <button
+            className="w-full text-slate-100 bg-indigo-400 
+                        hover:bg-indigo-600 flex items-center justify-center"
+            onClick={deletarServico}
+          >
+            {isLoading ? (
+              <ClipLoader color="#ffffff" size={24} />
+            ) : (
+              <span>Sim</span>
+            )}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DeletarServico;
