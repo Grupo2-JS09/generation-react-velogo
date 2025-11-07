2dbaf6fc704784f05db5499d9093470e08cd4063
Maristela Rocha <maristela.rocha.silva@gmail.com>
2025-11-07 07:05:41 -0300

    feat: Add page Contato

--- src/components/categoria/listacategoria/ListaCategoria.tsx
+++ src/components/categoria/listacategoria/ListaCategoria.tsx
@@ -28,20 +28,24 @@ function ListaCategorias() {
   return (
     <>
       <div className="min-h-screen bg-linear-to-b from-slate-800 via-slate-700 to-slate-900 text-white">
-        <div className="flex justify-center w-full py-4">
-          {isLoading && <SyncLoader color="#74b9ff" size={32} />}
-        </div>
-        <div className="flex justify-center w-full my-4">
-          <div className="container flex flex-col">
-            {!isLoading && categorias.length === 0 && (
-              <span className="text-3xl text-center my-8">
-                Nenhuma Categoria foi encontrada!
-              </span>
-            )}
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-              {categorias.map((categoria) => (
-                <CardCategoria key={categoria.id} categoria={categoria} />
-              ))}
+        <div className="h-screen py-5">
+          {isLoading && (
+            <div className="flex justify-center w-full my-8">
+              <SyncLoader color="#74b9ff" size={32} />
+            </div>
+          )}
+          <div className="flex justify-center w-full my-4">
+            <div className="container flex flex-col">
+              {!isLoading && categorias.length === 0 && (
+                <span className="text-3xl text-center my-8">
+                  Nenhuma Categoria foi encontrada!
+                </span>
+              )}
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                {categorias.map((categoria) => (
+                  <CardCategoria key={categoria.id} categoria={categoria} />
+                ))}
+              </div>
             </div>
           </div>
         </div>
